6240b8e8ff48af151302acf81bb16c7e833bc1a5
Rebecca Stewart <rebeccamstewart@live.com>
2020-01-21 15:31:27 -0800

    Added template file for twitter credentials

---
+++ Twitter Binary Classification with MLs and NNs/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access Twitter, please substitute xxxxxx for info from your own twitter account
+ACCESS_TOKEN ="xxxxxx"
+ACCESS_TOKEN_SECRET="xxxxxx"
+CONSUMER_KEY="xxxxxx"
+CONSUMER_SECRET="xxxxxx"
