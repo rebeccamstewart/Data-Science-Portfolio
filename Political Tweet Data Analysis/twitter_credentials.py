54a0a38dace2a02eaa7acaff50cae941be08cdbd
Rebecca Stewart <rebeccamstewart@live.com>
2020-01-23 13:27:36 -0800

    Political Tweets - Data (and Sentiment) Analysis

---
+++ Political Tweet Data Analysis/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access Twitter, replace xxxxxx with values from your own twitter account
+ACCESS_TOKEN ="xxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET="xxxxxxxxxxxxxxxxxxx"
+CONSUMER_KEY="xxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET="xxxxxxxxxxxxxxxxxxx"
